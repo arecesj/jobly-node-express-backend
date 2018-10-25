9489df192912111da280785db3a3ffebbf371626
JUAN ARECES <arecesj@gmail.com>
2018-10-25 09:34:24 -0700

    Morning commit

--- __tests__/unit/company_model.test.js
+++ __tests__/unit/company_model.test.js
@@ -31,16 +31,91 @@ beforeEach(async function() {
 });
 
 /** Get All Method */
-describe('User.getAll()', () => {
+describe('User.getAll() w/ no search terms', () => {
   it('Should get back all of the companies and responds with Company Data', async function() {
     const result = await Company.getAll();
 
-    expect(result.).toBe(1);
+    expect(result.length).toBe(1);
+    expect(result[0]).toHaveProperty('handle');
+    expect(result[0]).toHaveProperty('name');
+    expect(result[0].handle).toBe('JDAB');
+    expect(result[0].name).toBe('JD Areces and Bros, LLC');
   });
 });
 
-/** Search Method */
+/** Search Methods */
+describe('User.search() w/ just search', () => {
+  it('Should get back all of the companies and responds with Company Data', async function() {
+    const result = await Company.search({ search: 'Bros' });
 
+    expect(result.length).toBe(1);
+    expect(result[0]).toHaveProperty('handle');
+    expect(result[0]).toHaveProperty('name');
+    expect(result[0].handle).toBe('JDAB');
+    expect(result[0].name).toBe('JD Areces and Bros, LLC');
+  });
+});
+
+//Test that search returns error on with bad query
+describe('User.getAll() w/ just search and bad query', () => {
+  it('Should not get back all of the companies and responds with Company Data', async function() {
+    const result = await Company.search({ search: 'hhhhh' });
+
+    expect(result.length).toBe(0);
+  });
+});
+
+describe('User.getAll() w/ search and min', () => {
+  it('Should get back all of the companies and responds with Company Data', async function() {
+    const result = await Company.search({ search: 'Bros', min_employees: 1 });
+
+    expect(result.length).toBe(1);
+    expect(result[0]).toHaveProperty('handle');
+    expect(result[0]).toHaveProperty('name');
+    expect(result[0].handle).toBe('JDAB');
+    expect(result[0].name).toBe('JD Areces and Bros, LLC');
+  });
+});
+
+//Test that search returns error on a min and bad query
+describe('User.getAll() w/ search, a min and bad query', () => {
+  it('Should get back all of the companies and responds with Company Data', async function() {
+    const result = await Company.search({
+      search: 'juanito',
+      min_employees: 5
+    });
+
+    expect(result.length).toBe(0);
+  });
+});
+
+describe('User.getAll() w/ search and max', () => {
+  it('Should get back all of the companies and responds with Company Data', async function() {
+    const result = await Company.search({
+      search: 'bro',
+      max_employees: 300
+    });
+    expect(result.length).toBe(1);
+    expect(result[0]).toHaveProperty('handle');
+    expect(result[0]).toHaveProperty('name');
+    expect(result[0].handle).toBe('JDAB');
+    expect(result[0].name).toBe('JD Areces and Bros, LLC');
+  });
+});
+
+//Test that search returns error on a max and bad query
+describe('User.getAll() w/ search, a max and bad query', () => {
+  it('Should get back all of the companies and responds with Company Data', async function() {
+    const result = await Company.search({
+      search: 'juanito',
+      max_employees: 300
+    });
+
+    expect(result.length).toBe(0);
+  });
+});
+
+//TODO: Min + Max, Min, Max, Middleware MidMaxSize Function
 /** Create Method */
 
 /** Get One Method */
